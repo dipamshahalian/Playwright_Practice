08b7b333215513363753697eb36d030b6dcb78d9
dipamshahalian <dipam.shah@aliansoftware.com>
2025-04-24 17:55:41 +0530

    Synchronous and Asynchronous cases

--- sync.py
+++ sync.py
@@ -16,6 +16,6 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)  # Set headless to False to run in head mode
     page = browser.new_page()
-    page.goto("https://whatismyuseragent.com/")
+    page.goto("https://aliansoftware.com/")
     page.screenshot(path = "demo1.png")
     browser.close()
